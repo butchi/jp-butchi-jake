645bc735a9b9a6a3c47c9af3c815587193fa77ba
IWABUCHI Yuki <butchiyu+com-github@gmail.com>
2025-08-27 17:38:48 +0900

    編集UI実装

--- pages/custom/tel/index.vue
+++ pages/custom/tel/index.vue
@@ -2,13 +2,17 @@
   <v-main>
     <v-container class="pa-4">
       <h1>電話する</h1>
-      <v-form @submit.prevent="savePhoneNumber">
-        <v-text-field v-model="phoneNumber" label="電話番号を入力" type="tel" required></v-text-field>
-        <v-btn color="primary" @click="savePhoneNumber">保存</v-btn>
-      </v-form>
-      <div v-if="savedPhoneNumber">
+      <div v-if="!isEditing && savedPhoneNumber">
         <p>保存済み電話番号: <strong>{{ savedPhoneNumber }}</strong></p>
         <v-btn color="success" :href="`tel:${savedPhoneNumber}`">この番号に電話する</v-btn>
+        <v-btn color="primary" @click="startEdit">編集</v-btn>
+      </div>
+      <div v-else>
+        <v-form @submit.prevent="savePhoneNumber">
+          <v-text-field v-model="phoneNumber" label="電話番号を入力" type="tel" required></v-text-field>
+          <v-btn color="primary" @click="savePhoneNumber">保存</v-btn>
+          <v-btn color="default" @click="cancelEdit">キャンセル</v-btn>
+        </v-form>
       </div>
     </v-container>
   </v-main>
@@ -19,6 +23,7 @@ import { ref, onMounted } from 'vue'
 
 const phoneNumber = ref('')
 const savedPhoneNumber = ref('')
+const isEditing = ref(false)
 
 const storageKey = 'customTelNumber'
 
@@ -28,12 +33,23 @@ onMounted(() => {
     savedPhoneNumber.value = stored
     phoneNumber.value = stored
   }
+  isEditing.value = !stored;
 })
 
 function savePhoneNumber() {
   if (phoneNumber.value) {
     localStorage.setItem(storageKey, phoneNumber.value)
     savedPhoneNumber.value = phoneNumber.value
+    isEditing.value = false;
   }
+}
+
+function startEdit() {
+  isEditing.value = true;
+}
+
+function cancelEdit() {
+  phoneNumber.value = savedPhoneNumber.value;
+  isEditing.value = false;
 }
 </script>
